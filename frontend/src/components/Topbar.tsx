c50c19ea110a5129c0b903887c5584a025c4baf8
Sahil Shah <sahilshah2104@gmail.com>
2025-01-04 12:57:42 +0530

    Raw Topbar finalized

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,12 +1,18 @@
 export const Topbar = () => {
-    return <div className="flex justify-between">
-        <div className="text-2xl font-bold font-sans px-8 py-4">
-            <span className="text-yellow-200">Carbon</span>
-            <span className="text-green-600">Pulse</span>
+    return <div className="sticky top-0 flex justify-between mx-auto items-center bg-gray-300">
+        <div className="px-8 py-4 cursor-pointer">
+            <img 
+                src="/src/assets/cp-logo.png" 
+                alt="logo"
+                width={200}
+                height={100} 
+            />
         </div>
         <div className="flex space-x-2 px-8 py-4">
-            <span className="text-lg text-yellow-200 py-1">About</span>
-            <span className="rounded-2xl bg-green-600 text-lg cursor-pointer text-white px-5 py-1">
+            <span className="text-lg text-black py-1 px-5 cursor-pointer">
+                About
+            </span>
+            <span className="rounded-xl bg-black text-lg cursor-pointer text-white px-5 py-1">
                 Login
             </span>
         </div>

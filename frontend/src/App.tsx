4f09959d2938f13f7787e3d983fa666511edd568
Sahil Shah <sahilshah2104@gmail.com>
2025-01-13 23:01:35 +0530

    Completed hero section, started with main content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import './index.css'
+import { Hero } from './components/Hero'
 import { Topbar } from './components/Topbar'
 
 function App() {
   return (
     <div className='w-full h-full'>
       <Topbar />
+      <Hero />
     </div>
   )
 }
---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,23 @@
+export const Hero = () => {
+    return <div className="w-full">
+        <div className="text-black text-7xl text-center font-coustard font-bold tracking-tighter mt-14">
+            CarbonPulse
+        </div>
+        <div className="text-black text-lg text-center mt-2">
+            A Simpler Way Towards Adopting More Sustainable Environment!
+        </div>
+        <div>
+            <HeroImage />
+        </div>
+    </div>
+}
+
+const HeroImage = () => {
+    return <div className="w-full overflow-hidden px-6">
+        <img 
+            src="/src/assets/imgs/hero_image.jpg" 
+            alt="hero image"
+            className="w-full h-96 rounded-2xl shadow-2xl"
+        />
+    </div>
+}
